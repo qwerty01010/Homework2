cfe9ae6500889a80afdc0a0ea7439c7caa2179e2
qwerty01010 <110329988+qwerty01010@users.noreply.github.com>
2022-08-10 22:12:00 +0300

    Homework 2_3

---
+++ HW2_3.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine ("Введите число от 1 до 7 для проверки дня недели: ");
+String? numbString = Console.ReadLine ();
+var Numb = int.Parse (numbString!);
+
+if (Numb > 0 && Numb < 8 && Numb > 5 ) 
+{
+   if (Numb > 5 ) {Console.WriteLine ("Да");}
+   else {Console.WriteLine("Нет");}
+   
+}
+else 
+{
+    Console.WriteLine("Число не подходит для проверки дня недели");
+}
