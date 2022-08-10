8708017b05531c76d2003d451a997a4fd00cc505
IVANNNNN <gravemail@yandex.ru>
2022-08-10 21:37:59 +0300

    Homework 2_1

---
+++ HW2_1.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine ("Введите трехзначное число: ");
+String? numbString = Console.ReadLine ();
+var Numb = int.Parse (numbString!);
+int resultFirst = 0;
+
+if (Math.Abs (Numb) > 99 && Math.Abs (Numb) <1000 ) 
+{
+    resultFirst = (Numb/10) %10;
+    
+    if (resultFirst < 0) {resultFirst = resultFirst * -1;};
+    
+    Console.WriteLine (resultFirst);
+}
+else {Console.WriteLine ("Вы ввели не трехзначное число");}
