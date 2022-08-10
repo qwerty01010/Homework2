41243b078935979e6fee701ead205433aae57243
qwerty01010 <110329988+qwerty01010@users.noreply.github.com>
2022-08-10 21:55:25 +0300

    Add files via upload

---
+++ HW2_2.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine ("Введите число трехзначное или более: ");
+String? numbString = Console.ReadLine ();
+var Numb = int.Parse (numbString!);
+
+
+if (Math.Abs (Numb) > 99) 
+{
+    Console.WriteLine ("Третья цифра " + Numb.ToString ()[2]);
+}
+else {Console.WriteLine ("Третьей цифры нет");}
